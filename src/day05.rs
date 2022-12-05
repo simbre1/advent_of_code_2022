186179c3ca64d48542a4c085e7d474429a99b84d
simbre1 <stijn.imbrechts@uzleuven.be>
2022-12-05 17:47:30 +0100

    day 5

--- src/day05.rs
+++ src/day05.rs
@@ -24,6 +24,7 @@ impl std::str::FromStr for Move {
 pub fn solve() {
     println!("*** Day 05 ***");
 
+//    let s = std::fs::read_to_string("input/day05/example.txt").expect("peut");
     let s = std::fs::read_to_string("input/day05/input.txt").expect("peut");
     let (stacks, moves) = parse_input(&s);
 
@@ -34,27 +35,27 @@ pub fn solve() {
 
 fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<Move>) {
     let mut len = 0;
-    for line in input.lines() {
-        if !line.contains("[") {
-            break;
-        }
-        len = std::cmp::max(line.len(), len);
-    }
+    let stack_lines: Vec<&str> = input.lines()
+        .take_while(|line| line.contains("["))
+        .inspect(|line| len = std::cmp::max(len, line.len()))
+        .collect();
+
     let n = (len + 1)/4;
-    let mut stacks: Vec<Vec<char>> = (0..n).map(|_| Vec::new()).collect();
-    for line in input.lines() {
-        if !line.contains("[") {
-            break;
-        }
+    let mut stacks: Vec<Vec<char>> = (0..n)
+        .map(|_| Vec::new())
+        .collect();
+    for line in stack_lines {
         for i in 0..n {
             let j = (i * 4) + 1;
-            if j >= line.len() {
-                break;
+            if j < line.len() {
+                match line.chars().nth(j) {
+                    Some(c) => match c {
+                        'A'..='Z' => stacks[i].insert(0, c),
+                        _ => {}
+                    },
+                    _ => {}
+                }
             }
-            match line.chars().nth(j) {
-                Some(c) => match c { 'A'..='Z' => stacks[i].push(dbg!(c)), _ => {}},
-                _ => {}
-            };
         }        
     }
 
@@ -70,13 +71,47 @@ fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<Move>) {
 }
 
 fn part1(stacks: &Vec<Vec<char>>, moves: &Vec<Move>) {
+    let mut result = stacks.clone();
+
+    for m in moves {
+        for _ in 0..m.amount {
+            if !result[m.from-1].is_empty() {
+                let c = result[m.from-1].pop().unwrap();
+                result[m.to-1].push(c);    
+            }
+        }
+    }
+
+    let msg = stacks_to_string(&result);
+
     println!("* Part 1 *");
-    let count = 0;
-    println!("Count: {}", count);
+    println!("Msg: {}", msg);
 }
 
 fn part2(stacks: &Vec<Vec<char>>, moves: &Vec<Move>) {
+    let mut result = stacks.clone();
+
+    for m in moves {
+        let len = result[m.from-1].len() as u32;
+        for i in (len - m.amount)..len {
+            let c = result[m.from-1][i as usize];
+            result[m.to-1].push(c);
+        }
+        for _ in 0..m.amount {
+            result[m.from-1].pop();
+        }
+    }
+
+    let msg = stacks_to_string(&result);
+
     println!("* Part 2 *");
-    let count = 0;
-    println!("Count: {}", count);
+    println!("Msg: {}", msg);
+}
+
+fn stacks_to_string(stacks: &Vec<Vec<char>>) -> String {
+    stacks.iter()
+        .map(|s| s.last())
+        .filter(Option::is_some)
+        .map(Option::unwrap)
+        .collect::<String>()
 }
